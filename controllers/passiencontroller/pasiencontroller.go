fc62eca3bdc977e24828356b9af8a92a734ff37c
ahmjaelani <109496652+ahmjaelani@users.noreply.github.com>
2022-08-08 21:01:12 +0700

    Update pasiencontroller.go

--- controllers/passiencontroller/pasiencontroller.go
+++ controllers/passiencontroller/pasiencontroller.go
@@ -1,1 +1,23 @@
+package pasiencontroller
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Index(response http.Response, request *http.Request) {
+	fmt.Println("Its working")
+}
+
+func Add(response http.Response, request *http.Request) {
+
+}
+
+func Edit(response http.Response, request *http.Request) {
+
+}
+
+func Delete(response http.Response, request *http.Request) {
+
+}
 
